c2d15b022734f14733b623f1c64fc854766edd51
Yauheni_Tarhonski <Yauheni_Tarhonski@epam.com>
2019-07-01 17:55:34 +0300

    Studied theory today. Added only one small test.

--- TinyERP4FunTests/TinyERP4FunTests/ControllersTests/CountriesControllerTests.cs
+++ TinyERP4FunTests/TinyERP4FunTests/ControllersTests/CountriesControllerTests.cs
@@ -11,6 +11,7 @@ using TinyERP4Fun.Models.Common;
 using Xunit;
 using Tests;
 using Microsoft.EntityFrameworkCore;
+using TinyERP4Fun.Models;
 
 namespace Tests.TinyERP4FunTests
 {
@@ -20,29 +21,41 @@ namespace Tests.TinyERP4FunTests
         public async Task IndexViewResultNotNull()
         {
             // Arrange
- 
             var countriesList = GetTestCountries();
-
             var mockSet = SetUpMock.SetUpFor(GetTestCountries());
             var mock = new Mock<ICountriesService>();
             mock.Setup(c => c.GetIQueryable()).Returns(mockSet.Object);
             mock.Setup(c => c.GetListAsync()).Returns(Task.FromResult(GetTestCountries().AsEnumerable()));
-
             CountriesController controller = new CountriesController(mock.Object);
+
             // Act
             IActionResult result = await controller.Index(null);
 
             // Assert
             Assert.NotNull(result);
-
-
         }
+        [Fact]
+        public async Task IndexReturnsRightModel()
+        {
+            // Arrange
+            var countriesList = GetTestCountries();
+            var mockSet = SetUpMock.SetUpFor(GetTestCountries());
+            var mock = new Mock<ICountriesService>();
+            mock.Setup(c => c.GetIQueryable()).Returns(mockSet.Object);
+            mock.Setup(c => c.GetListAsync()).Returns(Task.FromResult(GetTestCountries().AsEnumerable()));
+            CountriesController controller = new CountriesController(mock.Object);
+
+            // Act
+            var result = (ViewResult) await controller.Index(null);
+            
+            // Assert
+            Assert.IsType<PaginatedList<Country>>(result.Model);
+        }
+
         private IQueryable<Country> GetTestCountries()
         {
             Country[] result = { new Country { Name = "Netherlands" },
                 new Country { Name = "Poland"},
-                new Country { Name = "Belarus"},
-                new Country { Name = "USA"},
                 new Country { Name = "Finland"}
             };
             return result.AsQueryable();
