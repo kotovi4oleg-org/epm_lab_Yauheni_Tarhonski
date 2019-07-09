56ea787ef819bb69ec5384049b882634a146b901
Yauheni_Tarhonski <Yauheni_Tarhonski@epam.com>
2019-07-09 16:47:57 +0300

    Add first worked test of service

--- TinyERP4FunTests/TinyERP4Fun.ModelServisesTests/CountriesServiceTests.cs
+++ TinyERP4FunTests/TinyERP4Fun.ModelServisesTests/CountriesServiceTests.cs
@@ -10,15 +10,52 @@ using Microsoft.Extensions.Configuration;
 using System.IO;
 using System.Reflection;
 using TinyERP4Fun.Interfaces;
-
+using Moq;
+using System.Linq;
 
 namespace TinyERP4FunTests.ModelServicesTests
 {
     public class CountriesServiceTests
     {
-        [Fact]
-        public void EmptyTest()
+        //public Contr ValidController { get; set; }
+        //public Contr NotValidController { get; set; }
+        public Mock<DefaultContext> Mock { get; set; }
+        public Mock<DbSet<Country>> MockSet { get; set; }
+        public Country singleEntity = new Country { Id = 2 };
+        public readonly IQueryable<Country> testEntities =
+            new Country[] {
+                        new Country {Id=0},
+                        new Country {Id=1},
+                        new Country {Id=2},
+                        new Country {Id=3},
+                        new Country {Id=4}
+                        }.AsQueryable();
+        public CountriesServiceTests()
         {
+            long Id = singleEntity.Id;
+            var mockSet = Tests.SetUpMock.SetUpFor(testEntities);
+
+            var options = new DbContextOptionsBuilder<DefaultContext>()
+                                .UseInMemoryDatabase(databaseName: "TestData")
+                                .Options;
+            var mock = new Mock<DefaultContext>(options);
+            mock.Setup(c => c.Set<Country>()).Returns(mockSet.Object);
+
+            Mock = mock;
+            MockSet = mockSet;
+        }
+        [Fact]
+        public async Task TryToTestService()
+        {
+            //Assert
+
+            CountriesService countriesServiceTests = new CountriesService(Mock.Object);
+
+            //Act
+            var result = await countriesServiceTests.GetAsync(2);
+
+            //Assert
+            Assert.Equal(result, testEntities.ToArray()[2]);
         }
     }
 }
